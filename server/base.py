1321ac077303b80a5fefb63a6dbda8611604113b
StardustLID <60168779+StardustLID@users.noreply.github.com>
2023-08-12 11:46:29 +0800

    rank spread pred backend

--- server/base.py
+++ server/base.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import pickle
+import numpy as np
+
 
 api = Flask(__name__)
+
+with open('rank_spread_pred.pkl', 'rb') as file:
+    reg = pickle.load(file)
 
 @api.route('/profile')
 def my_profile():
@@ -17,3 +23,17 @@ def epic():
         "about": "games"
     }
     return response_body  
+
+@api.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    rank0 = data['rank0']  # before
+    rank1 = data['rank1']  # after
+    spread_pred = reg.predict(np.array([rank0, rank1]).reshape(-1, 1))
+    spread_change = spread_pred[1] - spread_pred[0]
+    response = {
+        'spread0': spread_pred[0],
+        'spread1': spread_pred[1],
+        'spread_change': spread_change
+    }
+    return jsonify(response)
